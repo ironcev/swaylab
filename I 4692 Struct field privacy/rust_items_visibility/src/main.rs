c96f755d60b166442ef1389be6944d15b634e50b
Igor Rončević <ironcev@hotmail.com>
2024-03-06 11:58:50 +0100

    I 4692: Add example for using items from submodules

--- I 4692 Struct field privacy/rust_items_visibility/src/main.rs
+++ I 4692 Struct field privacy/rust_items_visibility/src/main.rs
@@ -3,4 +3,5 @@ mod top;
 fn main() {
     top::middle::use_private_middle_struct();
     top::middle::bottom::use_private_bottom_struct();
+    top::use_items_from_middle();
 }
--- I 4692 Struct field privacy/rust_items_visibility/src/top/mod.rs
+++ I 4692 Struct field privacy/rust_items_visibility/src/top/mod.rs
@@ -1,3 +1,5 @@
+use crate::top::middle::use_private_middle_struct;
+
 pub mod middle;
 
 struct PrivateTopStruct {
@@ -6,3 +8,7 @@ struct PrivateTopStruct {
 }
 
 const TOP_CONST: u64 = 0;
+
+pub fn use_items_from_middle() {
+    use_private_middle_struct();
+}
