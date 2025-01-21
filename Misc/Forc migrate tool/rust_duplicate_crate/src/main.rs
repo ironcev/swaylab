d79e7ee607c09c906895d32b3fe74df36f03dfbf
Igor Rončević <ironcev@hotmail.com>
2025-01-21 15:15:21 +0100

    Add `rust_duplicate_crate` to Forc migrate tool

---
+++ Misc/Forc migrate tool/rust_duplicate_crate/src/main.rs
@@ -0,0 +1,33 @@
+use duplicate::duplicate_item;
+
+struct S {
+    x: u8,
+    o: Option<u8>,
+}
+
+#[duplicate_item(
+    function     ref_type(type) reference(value) ref_mut(value);
+    [filter]     [&'a type]     [&value]         [value];
+    [filter_mut] [&'a mut type] [&mut value]     [ref mut value];
+)]
+fn function<'a, F>(s: ref_type([S]), f: F) -> Option<ref_type([u8])>
+where F: Fn(&ref_type([S])) -> bool + Copy + 'a
+{
+    if let Some(ref_mut([_x])) = s.o {
+        if f(&s) {
+            Some(reference([s.x]))
+        } else {
+            None
+        }
+    } else {
+        None
+    }
+}
+
+fn main() {
+    let mut s = S { x: 0, o: None };
+    let _ = filter(&s, |_| true);
+
+    let x = filter_mut(&mut s, |_| true).unwrap();
+    *x = 1;
+}
